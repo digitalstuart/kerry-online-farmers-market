4fdc60933983c7b3c4885a0e1b52c3270b6d2d5d
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-19 16:15:07 +0000

    Path for static files

--- market/settings.py
+++ market/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'products',
     'cart',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
