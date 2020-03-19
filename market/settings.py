ca60c0b817ea19c396760ca36f6292f65ae05137
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-19 10:35:26 +0000

    Deleted unnecessary workspace launch files

--- market/settings.py
+++ market/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'so5meo&@j%_f2cf%m+_em46$$vmi4*hf80ue56v^yn!m#5%s@g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
