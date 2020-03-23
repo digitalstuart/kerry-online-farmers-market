977cfbad26d5f489807d82aa3be446e39df12add
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-23 09:59:54 +0000

    Created form for new products to be added

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, include
-from .views import all_products, ProductCreate, ProductDelete, ProductUpdate
+from django.conf.urls import url
+from .views import all_products, add_or_edit_product
+
 
 urlpatterns = [
     url(r'^all/', all_products, name="products"),
-    url(r'^add_product/', ProductCreate.as_view(), name="create"),
-    url(r'^edit_product/', ProductUpdate.as_view(), name="update"),
-    url(r'^delete_product/', ProductDelete.as_view(), name="delete"),    
+    url(r'^new/$', add_or_edit_product, name="new_product"),
+    url(r'^(?P<pk>\d+)/edit/$', add_or_edit_product, name="edit_product")
 ]
--- products/views.py
+++ products/views.py
@@ -1,65 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from .forms import ProductForm
 
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
-class ProductCreate(CreateView):
-    model = Product
-    fields = ['name']
-
-class ProductUpdate(UpdateView):
-    model = Product
-    fields = ['name']
-
-class ProductDelete(DeleteView):
-    model = Product
-    success_url = reverse_lazy('product-list')
-
-
-
 # From the blog project - Creating and editing
-def create_or_edit_post(request, pk=None):
+def add_or_edit_product(request, pk=None):
     """
     Create a view that allows us to create
     or edit a post depending if the Post ID
     is null or not
     """
     # this line is used to check if there is a product (for editing purposes)
-    post = get_object_or_404(Post, pk=pk) if pk else None
+    products = Product.objects.all()
+    product = get_object_or_404(Product, pk=pk) if pk else None
     # this is for post (ignore for now)
     if request.method == "POST":
-        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        form = Product(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            post = form.save()
-            return redirect(post_detail, post.pk)
+            product = form.save()
+            return render(request, "products.html", {"products": products})
     # this is the get.  the instance=post is only important if we found a post earlier (for editing)
     else:
-        form = BlogPostForm(instance=post)
-    return render(request, 'blogpostform.html', {'form': form})
+        form = ProductForm(instance=product)
+    return render(request, 'product_form.html', {'form': form})
 
-
-# just for creating
-def create_post(request):
-                                            """
-                                            Create a view that allows us to create
-                                            or edit a post depending if the Post ID
-                                            is null or not
-                                            """
-                                    # this line can get removed as we only care about creating
-                                    # post = get_object_or_404(Post, pk=pk) if pk else None
-                                    # this is for post (ignore for now)
-                                    # if request.method == "POST":
-                                    #     form = BlogPostForm(request.POST, request.FILES, instance=post)
-                                    #     if form.is_valid():
-                                    #         post = form.save()
-                                    #         return redirect(post_detail, post.pk)
-                                    # # this is the get.  the instance=post is only important if we found a post earlier (for editing)
-                                    # else:
-
-#   THis is for just getting a form an putting it on the page.
-    form = BlogPostForm()
-    return render(request, 'blogpostform.html', {'form': form})
