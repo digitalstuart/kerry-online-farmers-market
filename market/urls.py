1e07c6bea90ec8bd84863dd57d2e619a68bfe4e5
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-24 14:29:44 +0000

    Search functionality added from course walkthrough project

--- market/urls.py
+++ market/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from products import urls as urls_products
 from cart import urls as urls_cart
 from checkout import urls as urls_checkout
+from search import urls as urls_search
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -28,6 +29,7 @@ urlpatterns = [
     url(r'^$', homepage, name='index'),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
     url(r'^checkout/', include(urls_checkout)),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
