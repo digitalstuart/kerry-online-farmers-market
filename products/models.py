3540071dae06bbace3ca7d2b28ddfb84f8237278
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-27 12:18:00 +0000

    Testing for adding seller history to profile page for logged in users

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')  
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, related_name="listings")
 
     def __str__(self):
         return self.name
