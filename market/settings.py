5ab8103313946b2f137d3685510937e1202bf031
digitalstuart <stuartrichardsdigital@gmail.com>
2020-03-31 16:08:02 +0000

    Changed email settings

--- market/settings.py
+++ market/settings.py
@@ -172,7 +172,7 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
