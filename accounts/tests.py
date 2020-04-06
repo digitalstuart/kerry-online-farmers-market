f0b7456c874dd8ce964dd633f32a8823d84d2649
digitalstuart <stuartrichardsdigital@gmail.com>
2020-04-06 11:39:06 +0000

    Tests written and successfully run for products model and accounts form

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .forms import UserLoginForm
 
-# Create your tests here.
+class UserLoginTest(TestCase):
+    
+    def test_valid_form(self):
+        form = UserLoginForm(data={
+            'username': 'admin', 
+            'password': 'testpassword1',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form = UserLoginForm(data={
+            'username': 'admin', 
+            'password': '',
+        })
+        self.assertFalse(form.is_valid())
--- products/tests.py
+++ products/tests.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
 from .models import Product
 
-class ProductTests(TestCase):
+class ProductTest(TestCase):
     """
     Here we'll define the tests that we'll run against our
     Product model
     """
-
     def test_str(self):
         test_name = Product(name='A product')
         self.assertEqual(str(test_name), 'A product')
