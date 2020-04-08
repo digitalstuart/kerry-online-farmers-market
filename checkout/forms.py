945926d603fbfeace57f71eee8fa6b6f884adc80
digitalstuart <stuartrichardsdigital@gmail.com>
2020-04-08 13:22:27 +0000

    Changed fields in checkout model/form for personal details form

--- checkout/forms.py
+++ checkout/forms.py
@@ -19,7 +19,6 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            'full_name', 'phone_number', 'country', 'postcode',
-            'town_or_city', 'street_address1', 'street_address2',
-            'county'
+            'name', 'phone_number', 'email_address', 'address_line_1', 
+            'address_line_2', 'town', 'county', 'eircode'
         )
--- checkout/models.py
+++ checkout/models.py
@@ -4,19 +4,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Order(models.Model):
-    full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=40, blank=False)
-    street_address2 = models.CharField(max_length=40, blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=25, blank=False)
+    email_address = models.EmailField(blank=False)
+    address_line_1 = models.CharField(max_length=40, blank=False)
+    address_line_2 = models.CharField(max_length=40, blank=False)
+    town = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
+    eircode = models.CharField(max_length=20, blank=True)
     date = models.DateField()
     user = models.ForeignKey(User)
 
     def __str__(self):
-        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        return "{0}-{1}-{2}".format(self.id, self.date, self.name)
 
 
 class OrderLineItem(models.Model):
