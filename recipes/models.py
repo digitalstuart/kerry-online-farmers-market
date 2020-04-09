50e51687890409e9c0f643106c05e21d8a3d2048
digitalstuart <stuartrichardsdigital@gmail.com>
2020-04-09 14:17:08 +0000

    Added default text to 'method' field as an indicator for users to give numbered recipe steps

--- recipes/models.py
+++ recipes/models.py
@@ -4,7 +4,7 @@ class Recipe(models.Model):
     name = models.CharField(max_length=254)
     image = models.ImageField(upload_to='images')  
     ingredients = models.TextField()
-    method = models.TextField()
+    method = models.TextField(default="1) ")
 
     def __str__(self):
         return self.name
