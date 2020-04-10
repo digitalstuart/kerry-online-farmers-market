43f38f6724f0a5e74c8257b2fd40d2b2218720a9
digitalstuart <stuartrichardsdigital@gmail.com>
2020-04-10 13:00:42 +0000

    Added 'help text' to field on payment details form

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,7 +8,7 @@ class MakePaymentForm(forms.Form):
     YEAR_CHOICES = [(i, i) for i in range(2020, 2036)]
 
     credit_card_number = forms.CharField(label='Card number', required=False)
-    cvv = forms.CharField(label='Security code (CVV)', required=False)
+    cvv = forms.CharField(label='Security code (CVV)', required=False, help_text='Last 3 numbers on back of card')
     expiry_month = forms.ChoiceField(label='Expiry month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Expiry year', choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
