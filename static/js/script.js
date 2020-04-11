bf02496168d9e8678000fd4be5501954d32bdbf2
digitalstuart <stuartrichardsdigital@gmail.com>
2020-04-11 11:12:45 +0000

    Javascript for 'back to top' floating button

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// Function for 'Back to top' button
+
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+document.getElementById("myBtn").addEventListener ("click", function () {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
